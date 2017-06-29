eab29513afaf0fdc622cdf4b594f6fa8ad7996f1
John Vascsinec <pensteeler@gmail.com>
2017-06-29 18:14:58 -0500

    Add files via upload

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,9 +1,10 @@
 module Players
-  class Human < Player
 
-    def move(board)
-      puts "Please enter 1-9:"
+  class Human < Player
+    def move( board )
+      puts "Please enter move:"
       gets.strip
     end
   end
+
 end
