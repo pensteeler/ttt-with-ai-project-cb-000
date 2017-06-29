a3f4bcca06f78e3bbf4272cb331c0f6fd42ef1f5
John Vascsinec <pensteeler@gmail.com>
2017-06-29 18:14:14 -0500

    Add files via upload

--- lib/board.rb
+++ lib/board.rb
@@ -1,13 +1,16 @@
 class Board
+
   attr_accessor :cells
 
-  def initialize()
-    reset!
-  end
 
-  def reset!
-    @cells = Array.new(9, " ")
-  end
+    def initialize
+      @cells = Array.new(9, " ")
+      #@board = board || Array.new(9, " ")
+    end
+
+    def reset!
+      @cells =  Array.new(9, " ")
+    end
 
   def display
     puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
@@ -17,27 +20,136 @@ class Board
     puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
-  def full?
-    cells.all?{|token| token == "X" || token == "O"}
-  end
+   def input_to_index( inputString )
+     inputString.to_i - 1
+   end
 
-  def turn_count
-    cells.count{|token| token == "X" || token == "O"}
-  end
+   def position( index )
+     @cells[index.to_i-1]
+   end
 
-  def valid_move?(input)
-    input.to_i.between?(1,9) && !taken?(input)
-  end
+   def full?
+     !@cells.include?(" ")
+   end
 
-  def update(input, player)
-    cells[input.to_i-1] = player.token
-  end
+   def turn_count
+     @cells.count{|token| token == "X" || token == "O"}
+   end
 
-  def position(input)
-    cells[input.to_i-1]
-  end
+   def taken?(index)
+     !(@cells[index.to_i-1].nil? || @cells[index.to_i-1] == " ")
+   end
 
-  def taken?(input)
-    !(position(input) == " " || position(input) == "")
-  end
+   def valid_move?(index)
+      index.to_i.between?(1,9) && !taken?( index )
+   end
+
+   def update( index, player )
+     #puts "Player.token:#{player.token}"
+     @cells[index.to_i-1] = player.token
+   end
+
+#*******************************************
+
+   def move(index, current_player)
+     @board[index] = current_player
+   end
+
+     def current_player
+       turn_count % 2 == 0 ? "X" : "O"
+     end
+
+
+     def turn
+       puts "Please enter 1-9:"
+       input = gets.strip
+       index = input_to_index(input)
+       if valid_move?( index)
+         move( index, current_player )
+         display_board
+       else
+         turn
+       end
+     end
+
+     def won?
+       # Return indexes if there is a win, otherwise nil
+       WIN_COMBINATIONS.each do |win_combination|
+         # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
+         # grab each index from the win_combination that composes a win.
+         win_index_1 = win_combination[0]
+         win_index_2 = win_combination[1]
+         win_index_3 = win_combination[2]
+
+         position_1 = @board[win_index_1] # load the value of the board at win_index_1
+         position_2 = @board[win_index_2] # load the value of the board at win_index_2
+         position_3 = @board[win_index_3] # load the value of the board at win_index_3
+
+         #puts "Checking Indexes: #{win_combination[0]}, #{win_combination[1]}, #{win_combination[2]}!"
+         #puts "Values: #{position_1}, #{position_2}, #{position_3}!"
+         if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+            (position_1 == "O" && position_2 == "O" && position_3 == "O")
+            #puts "Winning Indexes: #{win_combination[0]}, #{win_combination[1]}, #{win_combination[2]}!"
+            return win_combination
+         end
+
+       end  # do end
+
+       return false  # No winning combinations
+
+     end  # end won?
+
+
+     def draw?
+       # draw? expects false from won? however winner? expexcts nil
+       playerWon = won?
+       if playerWon == nil
+         playerWon = false
+       end
+
+       boardFull = full?
+
+         if (playerWon == false) && (boardFull == true)
+           true
+         elsif (playerWon == false) && (boardFull == false)
+           false
+         elsif playerWon == true
+           false
+         else
+           false
+         end
+     end  # end of draw?
+
+     def over?
+       if won? || draw? || full?
+         return true
+       else
+         return false
+       end
+     end
+
+     def winner
+         myWinner = won?
+
+         if myWinner != false
+           return @board[myWinner[0]]
+         else
+           return nil
+         end
+     end
+
+     def play
+         until over?
+           turn
+         end
+
+         if winner != nil
+           puts "Congratulations #{winner}!"
+         end
+
+         if draw?
+           puts "Cat's Game!"
+         end
+     end  # End of play
+
 end
--- lib/game.rb
+++ lib/game.rb
@@ -1,71 +1,100 @@
 class Game
-  attr_accessor :board, :player_1, :player_2
-  WIN_COMBINATIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [6,4,2]
-  ]
 
-  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
-    @board = board
-    @player_1 = player_1
-    @player_2 = player_2
-  end
+    attr_accessor :player_1, :player_2, :board, :winningToken
 
-  def over?
-    won? || draw?
-  end
+   WIN_COMBINATIONS = [
+     [0,1,2],
+     [3,4,5],
+     [6,7,8],
+     [0,3,6],
+     [1,4,7],
+     [2,5,8],
+     [0,4,8],
+     [2,4,6]
+   ]
 
-  def current_player
-    @board.turn_count % 2 == 0 ? @player_1 : @player_2
-  end
+   def initialize( player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board=Board.new )
+     @player_1 = player_1
+     @player_2 = player_2
+     @board = board
+     @winningToken = ""
+   end
 
-  def winner
-    if winning_combo = won?
-      @winner = @board.cells[winning_combo.first]
-    end
-  end
+   def current_player
+     @board.turn_count % 2 == 0 ? @player_1 : @player_2
+   end
 
-  def turn
-    player = current_player
-    current_move = player.move(@board)
-    if !@board.valid_move?(current_move)
-      turn
-    else
-      puts "Turn: #{@board.turn_count+1}\n"
-      @board.display
-      @board.update(current_move, player)
-      puts "#{player.token} moved #{current_move}"
-      @board.display
-      puts "\n\n"
-    end
-  end
+   #def full?
+    # !@board.cells.include?(" ")
+   #end
 
-  def play
-    while !over?
-      turn
-    end
-    if won?
-      puts "Congratulations #{winner}!"
-    elsif draw?
-      puts "Cats Game!"
-    end
-  end
+   def over?
+     won? || draw?# || full?
+   end
 
-  def won?
-    WIN_COMBINATIONS.detect do |combo|
-      @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]] &&
-      @board.taken?(combo[0]+1)
-    end
-  end
+   def won?
+     # Return indexes if there is a win, otherwise detect return nil
+     WIN_COMBINATIONS.detect do |win_combination|
 
-  def draw?
-    @board.full? && !won?
-  end
+       position_1 = @board.cells[win_combination[0]]
+       position_2 = @board.cells[win_combination[1]]
+       position_3 = @board.cells[win_combination[2]]
+
+       (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+          (position_1 == "O" && position_2 == "O" && position_3 == "O")
+          #puts "Winning Indexes: #{win_combination[0]}, #{win_combination[1]}, #{win_combination[2]}!"
+          # We found a winning combination
+     end
+
+   end # won?
+
+   def draw?
+     @board.full? && !won?
+   end
+
+   def winner
+     if winningMove = won?
+       @winningToken = @board.cells[winningMove[0]]
+       #puts "Winning Token:#{@winningToken}."
+       @winningToken
+     end
+   end
+
+   def valid_move?( index )
+     index.between?(0,8) && @board.cells[index] == " "
+   end
+
+   def turn
+
+       #@board.display
+
+       player = current_player
+       #print "\nCurrent player token:#{player.token}"
+       index = player.move( @board ).to_i - 1
+
+       if valid_move?( index )
+         @board.cells[index] = player.token
+       else
+         turn
+       end
+
+   end # end turn
+
+   def play
+
+     while !over?
+       turn
+     end
+
+     if draw?
+       puts "Cat's Game!"
+     end
+     if won?
+       winningMove = won?
+       #puts "Congratulations #{@board.cells[winningMove[0]]}!"
+       puts "Congratulations #{winner}!"
+     end
+
+   end
+
 end
