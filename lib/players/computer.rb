4e6f567e1731ec057c5d700e3bf5c0c5327148e2
John Vascsinec <pensteeler@gmail.com>
2017-06-29 20:11:05 -0500

    Add files via upload

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,10 +5,11 @@ module Players
 
       preferred_cells = ["5", "1", "3", "7", "9", "2", "4", "6", "8"]
       i=0
-      while board.cells[preferred_cells[i].to_i] == " "
+      #while board.cells[preferred_cells[i].to_i] != " "
+      while board.taken?( preferred_cells[i].to_i )
         i += 1
       end
-      move = prefered_cells[i]
+      move = preferred_cells[i]
 
     end
 
